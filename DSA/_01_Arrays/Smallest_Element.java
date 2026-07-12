4495bfdc4fa5e152491bd4bf556e4c7558255fe6
Ritesh Kumar Lenka <riteshkumarlenka2005@gmail.com>
2026-07-12 09:42:15 +0530

    Solved Smallest Element in an array problem with O(n) complexity

---
+++ DSA/_01_Arrays/Smallest_Element.java
@@ -0,0 +1,42 @@
+package DSA._01_Arrays;
+
+import java.util.*;
+
+public class Smallest_Element {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+
+        
+
+        int size = sc.nextInt();
+        
+        if(size<=0){
+            System.out.println("Please enter a valid positive size.");
+            sc.close();
+            return;
+        }
+
+        
+        int arr[] = new int[size];
+
+        System.out.println("Enter "+size+" elements:");
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+        for(int i=0;i<size;i++){
+            if(smallest>arr[i]){
+                smallest=arr[i];
+            }
+        }
+
+        System.out.println("Smallest element is "+smallest);
+
+
+        sc.close();
+
+    }
+}
